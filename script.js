6396a2777412f591ffb04ae61e1862626cd8aa4f
Jasmine <jayjadesanders@gmail.com>
2025-04-18 22:43:26 -0400

    Added ProductProperties class

---
+++ script.js
@@ -0,0 +1,13 @@
+// Part 1: Setting Up Classes
+
+console.log("Part 1: Setting Up Classes")
+
+class ProductProperties {
+    // Constructor method
+    constructor( name, price, quantity){
+        this.name = name,
+        this.price = price,
+        this.quantity = quantity
+    }
+
+}
